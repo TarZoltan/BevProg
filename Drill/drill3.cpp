41a9fb472aeaf848969429b2719889ac186be616
TarZoltan <90504669+TarZoltan@users.noreply.github.com>
2021-10-06 18:42:08 +0200

    Add files via upload

--- Drill/drill3.cpp
+++ Drill/drill3.cpp
@@ -3,7 +3,7 @@
 int main(){
 
 
-	double a = 0, b = 0, d = 0;
+	double a = 0, b = 0, d = 0, e = 0;
 	char c = 'a';
 	double small = 0, large = 1;
 	
@@ -35,7 +35,7 @@ int main(){
 	
 	
 	
-	cout << "Do you want to continue? Type any character if yes and type | if not!" <<endl;
+	cout << "Do you want to continue? Type a character if yes and type | if not!" << endl;
 	cin >> c;
 	
 	}
@@ -43,9 +43,11 @@ int main(){
 	
 	double min = 0, max = 0;
 	int i = 0;
-	string unit = "a";
 	
-	do {
+	/* 6. feladat
+	
+	
+	while (c != '|') {
 	
 	cout << "Enter a number!" << endl;
 	cin >> d;
@@ -64,13 +66,85 @@ int main(){
 		cout << d << " the largest so far" << endl;
 	}else{
 		cout << d << endl;
+	} 
+	
+	
+	
+	cout << "Do you want to continue? Type a character if yes and type | if not!" << endl;
+	cin >> c;
+	}*/
+	
+	string unit = "a";
+	
+	const double m_to_cm = 100;
+	const double inch_to_cm = 2.54;
+	const double ft_to_inch = 12;
+	double t = 0;
+	int j = 0;
+	double sum = 0;
+	
+	vector <double> v;
+	
+	
+	while (c != '|') {
+	
+	cout << "Enter a number!" << endl;
+	cin >> e >> unit;
+	
+	if(e >= 0){
+	
+		if (unit == "cm"){
+			t = e / m_to_cm;	
+		}else if(unit == "m"){
+			t = e;
+		}else if(unit == "in"){
+			t = (e * inch_to_cm)/m_to_cm;
+		}else if(unit == "ft"){
+			t = (e * ft_to_inch * inch_to_cm)/m_to_cm;
+		}else{
+			cout << "Wrong unit!" << endl;
+		}
+		
+		if (j == 0){
+			max = t;
+			min = t;
+			j = 1;
+		}
+		
+		sum = sum + t;
+		
+		if (t <= min){
+			min = t;
+		} else if (t >= max){
+			max = t;
+		}
+		
+		v.push_back(t);
+	
+	
+	}else{
+		cout << "Negative lenght!" << endl;
 	}
 	
 	
 	
-	cout << "Do you want to continue? Type any character if yes and type | if not!" <<endl;
+	cout << "Do you want to continue? Type a character if yes and type | if not!" << endl;
 	cin >> c;
-	}while (c != '|');
+	}
 
+	sort(v.begin(), v.end());
+	
+	
+	for(int l = 0; l < v.size(); l++){
+		cout << v[l] << endl;
+	}
+	
+
+	
+	cout << "Max value: " << max << " m" << endl;
+	cout << "Min value: " << min << " m" << endl;
+	cout << "Sum of values: " << sum << " m" << endl;
+	
+	
 	return 0;
 }
